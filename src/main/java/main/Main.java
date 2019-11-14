0c43bd0f143a6f3848e2ab2985e77f03f69447d8
pgvbm-iscteiul <pgvbm@iscte-iul.pt>
2019-11-14 23:23:31 +0000

    codigo com a GUI

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -16,8 +16,8 @@ public class Main {
 	
 
 	public static void main(String[] args) {
-		GUI g = new GUI();
-		g.open();
+		GUI gui = new GUI();
+		gui.open();
 		try {
 			FileInputStream excelFile = new FileInputStream(new File(FILE_NAME));
 			Workbook workbook = new XSSFWorkbook(excelFile);
