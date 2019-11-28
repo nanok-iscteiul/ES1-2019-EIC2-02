3210a4b578c7033b8344ab724db2eff00e9ddb9d
pgvbm-iscteiul <pgvbm@iscte-iul.pt>
2019-11-28 00:23:23 +0000

    commit com introdução dos valores dos indicadores de qualidade

--- src/main/java/main/GUI.java
+++ src/main/java/main/GUI.java
@@ -2,9 +2,11 @@ package main;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
+import java.awt.GridLayout;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.WindowConstants;
 
 public class GUI {
@@ -17,11 +19,28 @@ public class GUI {
 	}
 	
 	private void createFrame() {
+		
+		
 
         Container c = frame.getContentPane();
 
         JPanel panel = new JPanel();
-        panel.setLayout(new BorderLayout());
+        panel.setLayout(new GridLayout(0, 4));
+        
+        JTextField DCI = new JTextField("DCI");
+        DCI.setHorizontalAlignment(JTextField.CENTER);
+        JTextField DII = new JTextField("DII");
+        DII.setHorizontalAlignment(JTextField.CENTER);
+        JTextField ADCI = new JTextField("ADCI");
+        ADCI.setHorizontalAlignment(JTextField.CENTER);
+        JTextField ADII = new JTextField("ADII");
+        ADII.setHorizontalAlignment(JTextField.CENTER);
+        
+        
+        panel.add(DCI);
+        panel.add(DII);
+        panel.add(ADCI);
+        panel.add(ADII);
         
         c.add(panel);
 
