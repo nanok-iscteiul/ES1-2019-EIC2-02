be1c8dc275cc4a2e5949f33aa79eaefb0753303f
nanok-iscteiul <nanok@iscte-iul.pt>
2019-12-04 09:14:53 +0000

    Alterações na GUI

--- src/main/java/main/GUI.java
+++ src/main/java/main/GUI.java
@@ -3,7 +3,6 @@ package main;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -12,7 +11,6 @@ import java.awt.event.KeyEvent;
 import java.util.List;
 
 import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
