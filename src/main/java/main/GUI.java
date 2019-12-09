76a61d755e8babbf472bd0ad56039ca938eac6e9
stunt-iscteiul <stunt@iscte-iul.pt>
2019-12-09 17:54:53 +0000

    GUI- Melhorias.

--- src/main/java/main/GUI.java
+++ src/main/java/main/GUI.java
@@ -4,7 +4,10 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Desktop;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
@@ -57,27 +60,30 @@ public class GUI {
 	private JButton showFileButton;
 	private DefaultTableModel tableModel;
 	private JLabel definedRules;
+	private CostumGridBag costumGridBag;
 
 	public GUI(Application app) {
 		this.app = app;
 		frame = new JFrame("Aplicação");
 		createFrame();
+		addFields();
 		frame.setVisible(true);
 	}
 
 	private void createFrame() {
 
-		frame.setLayout(new GridLayout(1,2));
-		addFields2();
 		Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
-		frame.setSize(new Dimension((int)dimension.getWidth(),(int)dimension.getHeight()-50));
+		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		frame.setMinimumSize(new Dimension(1280, 720));
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 	}
-	
-	private void addFields2() {
-		//Painel esquerdo
-		JPanel esquerdo = new JPanel(new GridLayout(4,1));
+
+	private void addFields() {
 		Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+		
+		//Painel esquerdo
+		final JPanel esquerdo = new JPanel(new GridLayout(4,1));
+		//Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
 		esquerdo.setPreferredSize(new Dimension((int)dimension.getWidth()*(1/3), (int)dimension.getHeight()));
 		//painel esquerdo 1
 		JPanel esquerdo1 = new JPanel(new GridLayout(2,1));
@@ -85,6 +91,7 @@ public class GUI {
 		JPanel dataFile = new JPanel(new GridLayout(1,2));
 		JLabel dFText = new JLabel("Data File:");
 		dFText.setFont(new Font("Arial", Font.BOLD,20));
+		
 		dataFile.add(dFText);
 		
 		//painel load/show
@@ -281,6 +288,7 @@ public class GUI {
 				String choice = optionList.getSelectedItem().toString();
 				app.feature_envy(Double.parseDouble(atfd_txt.getText()), choice, Double.parseDouble(laa_txt.getText()));
 				app.defectDetectionDefinedRules(1);
+				
 			}
 		});
 		
@@ -412,7 +420,8 @@ public class GUI {
 		esquerdo.add(esquerdo4);
 		//Fim de painel Esquerdo
 		//Painel direito
-		JPanel direito = new JPanel(new GridLayout());
+		
+		final JPanel direito = new JPanel(new GridLayout());
 		direito.setPreferredSize(new Dimension((int)dimension.getWidth()*(2/3), (int)dimension.getHeight()));
 		String[] col = { "MethodId", "LOC", "CYCLO","ATFD", "LAA", "is_long_method", "iPlasma","PMD", 
 				"is_feature_envy", "is_long_method_by_defined_rules",
@@ -422,254 +431,11 @@ public class GUI {
 		table.setEnabled(false);
 	
 		direito.add(new JScrollPane(table));
-		//add à frame
-		frame.add(esquerdo);
-		frame.add(direito);
 		
-	}
+		//add à frame
 	
-
-	public void addFields() {
-		// Parte de cima
-		JPanel panel_aux1 = new JPanel();
-
-		filePath = new JTextField("Please Load An Exel file");
-		filePath.setHorizontalAlignment(JTextField.CENTER);
-		filePath.setEditable(false);
-		filePath.setPreferredSize(new Dimension(328, 50));
-		panel_aux1.add(filePath, BorderLayout.LINE_START);
-
-		JButton loadFileButton = new JButton("Load File");
-		loadFileButton.setHorizontalAlignment(JButton.CENTER);
-		loadFileButton.setPreferredSize(new Dimension(300, 50));
-		panel_aux1.add(loadFileButton);
-		loadFileButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				JFileChooser chooser = new JFileChooser();
-				if (chooser.showOpenDialog(frame) == JFileChooser.APPROVE_OPTION) {
-					app.setPath(chooser.getSelectedFile().getAbsolutePath());
-					filePath.setText(chooser.getSelectedFile().getName());
-					defectDetection.setEnabled(true);
-					featureEnvyButton.setEnabled(true);
-					longMethod.setEnabled(true);
-				}
-			}
-		});
-
-		frame.add(panel_aux1);
-		// Fim da Parte de cima
-
-		// Depois da Parte de cima
-		Border border = BorderFactory.createLineBorder(Color.BLUE, 1);
-
-		JPanel panel_aux2 = new JPanel();
-
-		JLabel linesOfCode = new JLabel("Lines of code");
-		linesOfCode.setHorizontalAlignment(JLabel.CENTER);
-		linesOfCode.setBorder(border);
-		linesOfCode.setPreferredSize(new Dimension(115, 30));
-		panel_aux2.add(linesOfCode);
-
-		loc = new JTextField("80");
-		loc.setHorizontalAlignment(JTextField.CENTER);
-		loc.setPreferredSize(new Dimension(30, 30));
-		panel_aux2.add(loc);
-		loc.addKeyListener(new KeyAdapter() {
-			public void keyTyped(KeyEvent keyEvent) {
-				char c = keyEvent.getKeyChar();
-				if (!(Character.isDigit(c))) {
-					keyEvent.consume();
-				}
-			}
-		});
-
-		JLabel cyclomaticComplexity = new JLabel("Cyclomatic Complexity");
-		cyclomaticComplexity.setHorizontalAlignment(JLabel.CENTER);
-		cyclomaticComplexity.setBorder(border);
-		cyclomaticComplexity.setPreferredSize(new Dimension(140, 30));
-		panel_aux2.add(cyclomaticComplexity);
-
-		cyclo = new JTextField("10");
-		cyclo.setHorizontalAlignment(JTextField.CENTER);
-		cyclo.setPreferredSize(new Dimension(30, 30));
-		panel_aux2.add(cyclo);
-		cyclo.addKeyListener(new KeyAdapter() {
-			public void keyTyped(KeyEvent keyEvent) {
-				char c = keyEvent.getKeyChar();
-				if (!(Character.isDigit(c))) {
-					keyEvent.consume();
-				}
-			}
-		});
-
-		longMethod = new JButton("Long Method");
-		longMethod.setPreferredSize(new Dimension(300, 50));
-		longMethod.setEnabled(false);
-		panel_aux2.add(longMethod);
-		longMethod.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if (loc.getText() != "" && cyclo.getText() != "") {
-					int locValue = Integer.parseInt(loc.getText());
-					int cycloValue = Integer.parseInt(cyclo.getText());
-					app.longMethod(locValue,cycloValue);
-				}
-			}
-		});
-
-		frame.add(panel_aux2);
-
-		// Antes da parte de baixo
-
-		JPanel panel_aux3 = new JPanel();
-
-		JLabel featureEnvy = new JLabel("Feature Envy");
-		featureEnvy.setHorizontalAlignment(JLabel.CENTER);
-		featureEnvy.setBorder(border);
-		featureEnvy.setPreferredSize(new Dimension(600, 50));
-		panel_aux3.add(featureEnvy);
-
-		frame.add(panel_aux3);
-
-		JPanel panel_aux4 = new JPanel();
-
-		JLabel atfd = new JLabel("ATFD >");
-		atfd.setHorizontalAlignment(JLabel.CENTER);
-		atfd.setBorder(border);
-		atfd.setPreferredSize(new Dimension(75, 30));
-		panel_aux4.add(atfd);
-
-		atfd_txt = new JTextField("4");
-		atfd_txt.setHorizontalAlignment(JTextField.CENTER);
-		atfd_txt.setPreferredSize(new Dimension(30, 30));
-		atfd_txt.addKeyListener(new KeyAdapter() {
-			public void keyTyped(KeyEvent keyEvent) {
-				char c = keyEvent.getKeyChar();
-				boolean exit = false;
-				if (c == KeyEvent.VK_PERIOD && atfd_txt.getText().contains(".")) {
-					keyEvent.consume();
-					exit = true;
-				}
-				if (c == KeyEvent.VK_PERIOD && !atfd_txt.getText().contains("."))
-					exit = true;
-				if (!(Character.isDigit(c)) && !exit)
-					keyEvent.consume();
-			}
-		});
-		panel_aux4.add(atfd_txt);
-
-		String[] option = { "and", "or" };
-		optionList = new JComboBox(option);
-		optionList.setSelectedIndex(1);
-		optionList.setPreferredSize(new Dimension(108, 30));
-		panel_aux4.add(optionList);
-
-		JLabel laa = new JLabel("LAA <");
-		laa.setHorizontalAlignment(JLabel.CENTER);
-		laa.setBorder(border);
-		laa.setPreferredSize(new Dimension(75, 30));
-		panel_aux4.add(laa);
-
-		laa_txt = new JTextField("0.42");
-		laa_txt.setHorizontalAlignment(JTextField.CENTER);
-		laa_txt.setPreferredSize(new Dimension(30, 30));
-		laa_txt.addKeyListener(new KeyAdapter() {
-			public void keyTyped(KeyEvent keyEvent) {
-				char c = keyEvent.getKeyChar();
-				boolean exit = false;
-				if (c == KeyEvent.VK_PERIOD && laa_txt.getText().contains(".")) {
-					keyEvent.consume();
-					exit = true;
-				}
-				if (c == KeyEvent.VK_PERIOD && !laa_txt.getText().contains("."))
-					exit = true;
-				if (!(Character.isDigit(c)) && !exit)
-					keyEvent.consume();
-			}
-		});
-		panel_aux4.add(laa_txt);
-
-		featureEnvyButton = new JButton("Feature Envy");
-		featureEnvyButton.setHorizontalAlignment(JButton.CENTER);
-		featureEnvyButton.setPreferredSize(new Dimension(300, 50));
-		featureEnvyButton.setEnabled(false);
-		panel_aux4.add(featureEnvyButton);
-		featureEnvyButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				String choice = optionList.getSelectedItem().toString();
-				app.feature_envy(Double.parseDouble(atfd_txt.getText()), choice, Double.parseDouble(laa_txt.getText()));
-			}
-		});
-
-		frame.add(panel_aux4);
-		// parte de baixo
-		JPanel panel_aux9 = new JPanel();
-		defectDetection = new JButton("Defect Defection");
-		defectDetection.setHorizontalAlignment(JButton.CENTER);
-		defectDetection.setEnabled(false);
-		defectDetection.setPreferredSize(new Dimension(600, 50));
-		panel_aux9.add(defectDetection);
-		defectDetection.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				app.defectDetection();
-			}
-		});
-
-		frame.add(panel_aux9);
-
-		JPanel panel = new JPanel();
-		panel.setLayout(new GridLayout(0, 4));
-		Dimension dim = new Dimension(200, 50);
-
-		JLabel DCI = new JLabel("DCI");
-		DCI.setHorizontalAlignment(JTextField.CENTER);
-		DCI.setBorder(border);
-		DCI.setPreferredSize(dim);
-		panel.add(DCI);
-
-		JLabel DII = new JLabel("DII");
-		DII.setHorizontalAlignment(JTextField.CENTER);
-		DII.setBorder(border);
-		DII.setPreferredSize(dim);
-		panel.add(DII);
-
-		JLabel ADCI = new JLabel("ADCI");
-		ADCI.setHorizontalAlignment(JTextField.CENTER);
-		ADCI.setBorder(border);
-		ADCI.setPreferredSize(dim);
-		panel.add(ADCI);
-
-		JLabel ADII = new JLabel("ADII");
-		ADII.setHorizontalAlignment(JTextField.CENTER);
-		ADII.setBorder(border);
-		ADII.setPreferredSize(dim);
-		panel.add(ADII);
-
-		dciIplasma = new JTextField("Load File");
-		dciIplasma.setHorizontalAlignment(JTextField.CENTER);
-		dciIplasma.setEditable(false);
-		dciIplasma.setBorder(border);
-		panel.add(dciIplasma);
-
-		diiIplasma = new JTextField("Load File");
-		diiIplasma.setHorizontalAlignment(JTextField.CENTER);
-		diiIplasma.setEditable(false);
-		diiIplasma.setBorder(border);
-		panel.add(diiIplasma);
-
-		adciIplasma = new JTextField("Load File");
-		adciIplasma.setHorizontalAlignment(JTextField.CENTER);
-		adciIplasma.setEditable(false);
-		adciIplasma.setBorder(border);
-		panel.add(adciIplasma);
-
-		adiiIplasma = new JTextField("Load File");
-		adiiIplasma.setHorizontalAlignment(JTextField.CENTER);
-		adiiIplasma.setEditable(false);
-		adiiIplasma.setBorder(border);
-		panel.add(adiiIplasma);
-
-		frame.add(panel);
-
+		frame.setContentPane(new CostumGridBag(esquerdo, direito));
+		
 	}
 
 	public void receiveOutputDefectDetection(int [] countersIPlasma, int [] countersPmd) {
@@ -685,7 +451,7 @@ public class GUI {
 	}
 	
 	public void receiveOutputDefectDetectionDefinedRules(String origem,int [] counters) {
-		definedRules.setText("Defined Rules("+origem+")");
+		definedRules.setText(origem);
 		
 		dciDefinedRules.setText(""+ counters[0]);
 		diiDefinedRules.setText(""+ counters[1]);
