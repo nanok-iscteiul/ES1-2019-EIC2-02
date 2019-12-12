bd349780e2e0208d5e6b4b245559c5087c40f4c1
pgvbm-iscteiul <pgvbm@iscte-iul.pt>
2019-12-12 23:37:06 +0000

    Javadoc para a classe MethodData.

--- src/main/java/application/MethodData.java
+++ src/main/java/application/MethodData.java
@@ -1,5 +1,9 @@
 package application;
 
+/**
+ * @author ESI-2019-EIC2-02
+ *
+ */
 public class MethodData {
 	private int methodId;//id do metodo obtido do excel
 	private String packageName, className, methodName;// nome do package, da classe, e nome do metodo obtidos do excel
@@ -24,68 +28,127 @@ public class MethodData {
 		this.pmd = pmd;
 	}
 
-	public void setIs_long_method_by_rules(boolean set) {// definir o valor do campo que indica se o metodo é ou não
-															// long method pelas regras definidas
+	/**
+	 * Defines the value of the field that points if the method 
+	 * is or isn't a long method by the rules specified
+	 * @param sets the value of the specified field
+	 */
+	public void setIs_long_method_by_rules(boolean set) {
 		is_long_method_by_rules = set;
 	}
 
-	public void setIs_feature_envy_by_rules(boolean set) {// definir o valor do campo que indica se o método é ou não
-															// feature_envy pelas regras definidas
+	/**
+	 * Defines the value of the field that points if the method 
+	 * is or isn't a feature envy method by the rules specified
+	 * @param sets the value of the specified field
+	 */
+	public void setIs_feature_envy_by_rules(boolean set) {
 		is_feature_envy_by_rules = set;
 	}
 
+	/**
+	 * @return the id of the method obtained from the Excel file
+	 */
 	public int getMethodId() {
 		return methodId;
 	}
 
+	/**
+	 * @return the name of the Package
+	 */
 	public String getPackageName() {
 		return packageName;
 	}
 
+	/**
+	 * @return the name of the Class
+	 */
 	public String getClassName() {
 		return className;
 	}
 
+	
+	/**
+	 * @return the method name
+	 */
 	public String getMethodName() {
 		return methodName;
 	}
 
+	/**
+	 * @return the number of lines of code
+	 */
 	public int getLoc() {
 		return loc;
 	}
 
+	/**
+	 * @return the cyclomatic complexity
+	 */
 	public int getCyclo() {
 		return cyclo;
 	}
 
+	/**
+	 * @return the number of accesses of
+	 * the method to methods of other Classes
+	 */
 	public double getAtfd() {
 		return atfd;
 	}
 
+	/**
+	 * @return the number of accesses of
+	 * the method to attributes of own Class
+	 */
 	public double getLaa() {
 		return laa;
 	}
 
+	/**
+	 * @return true if is_long_method
+	 * 		   false otherwise
+	 */
 	public boolean getIs_long_method() {
 		return is_long_method;
 	}
 
+	/**
+	 * @return true if is_feature_envy
+	 * 		   false otherwise
+	 */
 	public boolean getIs_feature_envy() {
 		return is_feature_envy;
 	}
 
+	/**
+	 * @return true is is iPlasma
+	 * 		   false otherwise
+	 */
 	public boolean getIplasma() {
 		return iplasma;
 	}
 
+	/**
+	 * @return true if is Pmd
+	 * 		   false otherwise
+	 */
 	public boolean getPmd() {
 		return pmd;
 	}
 
+	/**
+	 * @return true is is_long_method_by_rules
+	 * 		   false otherwise
+	 */
 	public boolean getIs_long_method_by_rules() {
 		return is_long_method_by_rules;
 	}
 
+	/**
+	 * @return true id is_feature_envy_by_rules
+	 * 		   false otherwise
+	 */
 	public boolean getIs_feature_envy_by_rules() {
 		return is_feature_envy_by_rules;
 	}
